1062260e9f4976e0055378107415a23c34e2bad7
Jukka Huhtamäki <jukka.huhtamaki@tut.fi>
2015-09-21 10:59:19 +0300

    Polishing crawler code layout

--- crawl-indiegogo.js
+++ crawl-indiegogo.js
@@ -10,11 +10,15 @@ var casper = require('casper').create({
 var fs = require('fs');
 var clicks = 0;
 var x = require('casper').selectXPath; casper.start('https://www.indiegogo.com/projects/ jolla-tablet-world-s-first-crowdsourced-tablet #/funders');
+
 function clickAndSave() {
-fs.write('data/01-raw/temp.html', this.getHTML('html'), 'w');
-// http://www.codigomanso.com/en/2010/07/simple- javascript-formatting-zero-padding/
-console.log('Click #' + clicks); this.click(x("//a[text()='show more']")); clicks += 1; fs.write('data/01-raw/investments-sondor-click'
-+ ('00000' + clicks).slice (-5) + '.html', this.getHTML('desktop-funders'), 'w');
-this.wait(1000*10, function() {});
-this.run(clickAndSave); }
+  fs.write('data/01-raw/temp.html', this.getHTML('html'), 'w');
+  // http://www.codigomanso.com/en/2010/07/simple- javascript-formatting-zero-padding/
+  console.log('Click #' + clicks); 
+  this.click(x("//a[text()='show more']")); 
+  clicks += 1; 
+  fs.write('data/01-raw/investments-sondor-click' + ('00000' + clicks).slice (-5) + '.html', this.getHTML('desktop-funders'), 'w');
+  this.wait(1000*10, function() {});
+  this.run(clickAndSave); 
+}
 casper.run(clickAndSave);
